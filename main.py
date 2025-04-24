3566478379973a22dc5e397c158d15e03cb534fd
mrDem52 <93878095+mrDem52@users.noreply.github.com>
2025-04-24 21:56:38 +0300

    les2

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return '<h1>Hello World</h1>'
+    return render_template('index.html')
 
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+
+@app.route('/about')
+def user():
+    return render_template('about.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
