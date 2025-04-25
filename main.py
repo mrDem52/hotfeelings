f20c1d15897e109e9afd431f4f6ec6d9b9ce2d1f
mrDem52 <93878095+mrDem52@users.noreply.github.com>
2025-04-25 23:25:44 +0300

    les3 (SQL)

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:q@localhost/hf'
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nulllable=False)
+    intro = db.Column(db.String(300), nulllable=False)
+    text = db.Column(db.Text, nulllable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
 
 
 @app.route('/')
